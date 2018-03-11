6d466b672ac9813034d953b414ce8b5a4b928da3
Hang Qi <qihang001@outlook.com>
2018-03-11 17:50:03 +1100

    callback message added

--- BankPayment.WebApp/ClientApp/app/components/payment/payment.component.ts
+++ BankPayment.WebApp/ClientApp/app/components/payment/payment.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonService } from '../../shared/common.service';
+import { AbstractControl } from '@angular/forms/src/model';
 
 @Component({
     selector: 'payment'
@@ -10,19 +11,44 @@ import { CommonService } from '../../shared/common.service';
 export class PaymentComponent {
     formGroup: FormGroup;
 
-    initPayment() {
-        let tokens = document.getElementsByName('__RequestVerificationToken');
-        let token: string | null = null;
-        if (tokens.length > 0) {
-            token = tokens.item(0).getAttribute('value');
-        }
-        this.paymentInfo = {
-            bsb: this.formGroup.controls['bsb'].value as string,
-            accountName: this.formGroup.controls['accountName'].value as string,
-            accountNumber: this.formGroup.controls['accountNumber'].value as string,
-            reference: this.formGroup.controls['reference'].value as string,
-            amount: this.formGroup.controls['amount'].value as number,
-            __RequestVerificationToken: token == null ? '' : token.toString()
+    MessageStyles: Map<boolean, string> = new Map<boolean, string>();
+
+    messageStyle?: string;
+
+    private _returnMessage: string = '';
+
+    set returnMessage(msg: string) {
+        this._returnMessage = msg;
+        setTimeout(() => {
+            this._returnMessage = '';
+        }, 2000);
+    }
+
+    get returnMessage(): string {
+        return this._returnMessage;
+    }
+
+    initPayment(clearFrom: boolean) {
+        let controls = this.formGroup.controls as { [key: string]: AbstractControl };
+
+        if (!clearFrom) {
+            let tokens = document.getElementsByName('__RequestVerificationToken');
+            let token: string | null = null;
+            if (tokens.length > 0) {
+                token = tokens.item(0).getAttribute('value');
+            }
+            
+            this.paymentInfo = {
+                bsb: controls['bsb'].value as string,
+                accountName: controls['accountName'].value as string,
+                accountNumber: controls['accountNumber'].value as string,
+                reference: controls['reference'].value as string,
+                amount: controls['amount'].value as number,
+                __RequestVerificationToken: token == null ? '' : token.toString()
+            }
+        } else {
+            this.paymentInfo = {};
+            this.formGroup.reset();
         }
     }
 
@@ -35,7 +61,7 @@ export class PaymentComponent {
             ])],
             'accountNumber': [null, Validators.compose([
                 Validators.required
-                , Validators.pattern(/\d+/)
+                , Validators.pattern(/^\d+$/)
             ])],
             'accountName': [null, Validators.required],
             'reference': [null, Validators.maxLength(20)],
@@ -44,26 +70,35 @@ export class PaymentComponent {
                 , Validators.min(1)
             ])]
         });
+
+        this.MessageStyles.set(true, 'payment-success');
+        this.MessageStyles.set(false, 'payment-warning');
     }
 
     paymentInfo: IPaymentInfo;
 
     submitPayment = () => {
         if (this.formGroup.valid) {
-            this.initPayment();
+            this.initPayment(false);
             //console.log(this.paymentInfo);
             this.commonService.postForm('./api/WhatEver/SavePayment'
                 , this.paymentInfo
                 , (val) => {
                     let res = val.json() as IPaymentSaveResponse;
+                    this.returnMessage = (res.errors as string[])[0];
+                    this.messageStyle = this.MessageStyles.get(res.success);
                     if (res.success) {
                         console.log((res.errors as string[])[0]);
+                        // todo
+                        this.initPayment(true);
                     } else {
                         console.error(res.errors);
+                        // todo
                     }
                 });
         }
     }
+
 }
 interface IAntiForgery {
     __RequestVerificationToken?: string;
@@ -73,10 +108,10 @@ interface IPaymentInfo extends IAntiForgery {
     accountNumber?: string;
     accountName?: string;
     reference?: string;
-    amount?: number;    
+    amount?: number;
 }
 interface IJsonResponse {
-    success?: boolean;
+    success: boolean;
     errors?: string[];
 }
 interface IPaymentSaveResponse extends IJsonResponse {
